5904e9bc164110b4693853b7b4ca2ca53cefcd8c
Ilman Dewantara <44839852+ilmndwntr@users.noreply.github.com>
2021-03-07 21:31:57 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+// X-DARK CHANNEL
+
+$UserAgent= "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)";
+$Addres = "3M3GyHfnn56bXjUiRcrnns2V9kWmNYj4C5";
+
+$Cookie = "PHPSESSID=o08v9m244cgud86g92gaqt8k55; _ga=GA1.2.1243436230.1615114043; _gid=GA1.2.1700676595.1615114043; __tawkuuid=e::coinez.biz::QV3JqmOxTIcSrE27V+QbALlLlhg9VhwZiFYyYkLoffV5Y3ogiBwIlUI5EU0eoEgK::2; _gat=1; TawkConnectionTime=0";
+
+//Password = suksesslalubosquekamukerenbuangets
